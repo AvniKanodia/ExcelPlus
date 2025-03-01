3a0fd7f1ecd466c59271678c3022fa7556546d8f
Avni Kanodia <avnikanodia06@gmail.com>
2025-02-28 19:18:26 -0600

    Creating new statistics namespace

---
+++ includes/statistics.hpp
@@ -0,0 +1,9 @@
+#ifndef STATISTICS_HPP
+#define STATISTICS_HPP
+#include <vector>
+#include <string>
+
+namespace Statistics {
+
+}
+#endif 
---
+++ src/statistics.cc
@@ -0,0 +1,9 @@
+#include "statistics.hpp"
+#include "statistics.hpp"
+#include <stdexcept>
+#include <iostream>
+#include <vector>
+
+namespace statistics {
+    
+}
